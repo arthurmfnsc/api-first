dd00a15b61acb4f4164a77c15e1ce3a77108771e
Arthur Fonseca <144041324+arthurmfnsc@users.noreply.github.com>
2024-04-03 22:26:31 -0300

    🎉 First commit

---
+++ src/test/java/com/arthurfnsc/apifirst/ApiFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthurfnsc.apifirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
