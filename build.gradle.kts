8f967b7903169df509be109362a38e9b7619e90a
Arthur Fonseca <144041324+arthurmfnsc@users.noreply.github.com>
2024-04-08 03:18:18 -0300

    🔧 Add SonarQube configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ plugins {
 
 	id("io.spring.dependency-management") version "1.1.4"
 	id("org.openapi.generator") version "7.4.0" apply false
+	id("org.sonarqube") version "5.0.0.4638" apply false
 	id("org.springframework.boot") version "3.2.4"
 }
 
 apply(from = "plugins/java.gradle")
 apply(from = "plugins/openapi.gradle")
+apply(from = "plugins/sonarqube.gradle")
 
 group = "com.arthurfnsc"
 version = "0.0.1-SNAPSHOT"
