34b1e1e248d13f7ee70fadef951050d55830951b
Arthur Fonseca <144041324+arthurmfnsc@users.noreply.github.com>
2024-04-08 04:00:25 -0300

    🚨 Fix TAB identation (#26)

--- src/main/java/com/arthurfnsc/apifirst/ApiFirstApplication.java
+++ src/main/java/com/arthurfnsc/apifirst/ApiFirstApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiFirstApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFirstApplication.class, args);
 	}
-
 }
